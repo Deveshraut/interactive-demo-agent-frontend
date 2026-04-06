b5dc095f34d30904752c845a5a2a9912e1d110ec
Devesh-Raut <devesh.raut@aetherti.com>
2026-04-06 21:20:17 +0530

    changes done in workflow selector component

--- src/app/core/components/workflow-selector/workflow-selector.component.ts
+++ src/app/core/components/workflow-selector/workflow-selector.component.ts
@@ -5,8 +5,8 @@ import { WorkflowSummary } from '../../models/demo-events.models';
 @Component({
   selector: 'app-workflow-selector',
   standalone: true,
-  templateUrl: './WorkflowSelectorComponent.component.html',
-  styleUrl: './WorkflowSelectorComponent.component.scss'
+  templateUrl: './workflow-selector.component.html',
+  styleUrl: './workflow-selector.component.scss'
 })
 export class WorkflowSelectorComponent {
   workflows = input.required<WorkflowSummary[]>();
